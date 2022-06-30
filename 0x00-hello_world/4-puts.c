67331068d186cf7cbcc4bfd92a04b2162ddec394
Riyadh <riyadhmuhdin@gmail.com>
2022-06-30 15:00:55 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 after printing the function
+ * Return: 0 after printing the function
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
